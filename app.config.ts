4b377111497ae8812c0ed310b3a77ec503a15fd7
Adam Hari <adam.hari@kashoo.com>
2023-10-19 09:01:22 -0600

    build: expo prebuild

---
+++ app.config.ts
@@ -0,0 +1,34 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+import { bundleIdentifier, version, buildNumber, name } from "./package.json";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  name,
+  slug: name,
+  version,
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    bundleIdentifier: bundleIdentifier,
+    buildNumber: buildNumber.toString(),
+    supportsTablet: true,
+  },
+  android: {
+    package: bundleIdentifier,
+    versionCode: buildNumber,
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+    bundler: "metro",
+  },
+});
