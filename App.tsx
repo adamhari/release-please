e2bb078131fbc7166541ebe09b1da558ba381821
Adam Hari <adamjhari@outlook.com>
2024-06-12 13:26:42 -0600

    feat: make it white!

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
         backgroundColor: "black",
       }}
     >
-      <Text style={{ color: "orange" }}>Release Please!</Text>
+      <Text style={{ color: "white" }}>Release Please!</Text>
       <StatusBar style="auto" />
     </View>
   );
