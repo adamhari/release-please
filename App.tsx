696e0bb0a9cfa8c5e56f61cb582ca3ddb0f12b6c
Adam Hari <adamjhari@outlook.com>
2024-06-18 12:53:26 -0600

    feat: make it blue!

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "blue",
+        backgroundColor: "orange",
       }}
     >
       <Text style={{ color: "white" }}>Release Please!</Text>
